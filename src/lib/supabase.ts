1cbaf14aa5609bfaa89c08f9ff349993c674ca7c
estellagu <126267993+themagicfrog@users.noreply.github.com>
2025-11-24 12:21:10 -0500

    init

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+export const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL || '';
+export const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY || '';
+
+// import { createClient } from '@supabase/supabase-js';
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
